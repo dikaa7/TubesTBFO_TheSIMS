569bd1eea1c3242ef5ce9a595065323cfac31c9b
Ade Surya <55965490+lembahdata@users.noreply.github.com>
2019-10-01 23:49:32 -0400

    Update play.py

--- src/play.py
+++ src/play.py
@@ -1,6 +1,30 @@
+# Nama File : play.py
+# Tanggal: 19 September 2019
+# Topik: Tubes TBFO
+# Spesifikasi : Program utama, menjalankan keseluruhan fungsi
+# Programmer : 
+# Ade Surya Handika	- 13518007
+# Muhammad Rikzi Ismail - 13518148
+
 import sims as s
-
+print("====================================")
+print("    ████████╗██╗  ██╗███████╗     ")
+print("    ╚══██╔══╝██║  ██║██╔════╝     ")
+print("       ██║   ███████║█████╗       ")
+print("       ██║   ██╔══██║██╔══╝       ")
+print("       ██║   ██║  ██║███████╗     ")
+print("   ███████╗██╗███╗═╝╚███╗███████╗")
+print("   ██╔════╝██║████╗ ████║██╔════╝")
+print("   ███████╗██║██╔████╔██║███████╗")
+print("   ╚════██║██║██║╚██╔╝██║╚════██║")
+print("   ███████║██║██║ ╚═╝ ██║███████║")
+print("   ╚══════╝╚═╝╚═╝     ╚═╝╚══════╝")
+print("====================================")
+print(" T H E   S I M S  S I M U L A T O R ")
+print("====================================")
+print("## DIBUAT OLEH :")
+print("# Ade Surya Handika     - 13518007")
+print("# Muhammad Rikzi Ismail - 13518148")
+print("====================================")	
 player = s.Status(0,10,0)
-player.TulisStatus()
 player.BacaMenu()
-
