3d284a7dddf848a28a44f6d3cd462fc68848d134
Ade Surya <55965490+lembahdata@users.noreply.github.com>
2019-10-01 23:48:25 -0400

    Update constant.py

--- src/constant.py
+++ src/constant.py
@@ -1,3 +1,11 @@
+# Nama File : constant.py
+# Tanggal: 19 September 2019
+# Topik: Tubes TBFO
+# Spesifikasi : Program berisi konstanta nilai MAX dan MIN
+# Programmer : 
+# Ade Surya Handika		- 13518007
+# Muhammad Rikzi Ismail - 13518148
+
 MINENERGY = 0
 MINHYGIENE = 0
 MINFUN = 0
